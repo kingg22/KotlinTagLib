fa21ff42133a4d79deb54f873ec721c195462e50
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-15 16:07:36 -0500

    build(KotlinTagLib): refactor to kmp and put android and jvm targets
    TODO add native target with C/c++ interop

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,57 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.multiplatform)
 }
 
 kotlin {
     compilerOptions {
-        apiVersion = KotlinVersion.KOTLIN_2_0
-        languageVersion = KotlinVersion.KOTLIN_2_0
-        jvmTarget = JvmTarget.JVM_1_8
+        apiVersion.set(KotlinVersion.KOTLIN_2_0)
+        languageVersion.set(KotlinVersion.KOTLIN_2_0)
+        extraWarnings.set(true)
+        allWarningsAsErrors.set(true)
+        freeCompilerArgs.add("-Xexpect-actual-classes")
     }
-    explicitApi()
-}
 
-dependencies {
-    androidTestImplementation(libs.androidx.runner)
-    androidTestImplementation(libs.androidx.rules)
+    explicitApi()
+
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    applyDefaultHierarchyTemplate {
+        common {
+            group("androidAndJvm") {
+                withAndroidTarget()
+                withJvm()
+            }
+        }
+    }
+
+    androidTarget {
+        publishLibraryVariants("release")
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+
+    jvm {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+
+    sourceSets {
+        androidInstrumentedTest.dependencies {
+            implementation(libs.androidx.runner)
+            implementation(libs.androidx.rules)
+        }
+    }
 }
 
 android {
     namespace = "com.kyant.taglib"
+    // https://developer.android.com/tools/releases/platforms
     compileSdk = 36
     buildToolsVersion = "36.0.0"
     ndkVersion = "29.0.13599879"
